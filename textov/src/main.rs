253222a39c03f6df35bdd1079769b4f56ff54299
pravshot <praveenkalva@icloud.com>
2022-04-20 02:08:04 -0500

    added goldilocks and the three bears

--- textov/src/main.rs
+++ textov/src/main.rs
@@ -1,8 +1,8 @@
 use textov::Textov;
 
 fn main() {
-    let tev = Textov::new("datasets/green-eggs-and-ham.txt".to_string());
+    let tev = Textov::new("datasets/goldilocks.txt".to_string());
     tev.print_all();
-    println!("{}", tev.generate_text(5));
+    println!("{}", tev.generate_text(4));
 
 }
