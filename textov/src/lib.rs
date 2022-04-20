b5d3dee6463305dfbb55911f5539311c823daa39
pravshot <praveenkalva@icloud.com>
2022-04-20 01:40:16 -0500

    restructed code and now have a working model with markov order-1

--- textov/src/lib.rs
+++ textov/src/lib.rs
@@ -1,96 +1,6 @@
 extern crate rand;
 
 use std::collections::HashMap;
-
-use rand::Rng;
-
-pub struct Textov {
-    num_unique_phrases: usize,
-    markov_matrix: Vec<Vec<f64>>,
-    phrase_idx_map: HashMap<String, usize>,
-    idx_phrase_map: HashMap<usize, String>,
-    sentences: Vec<String>
-    //k_order: enum maybe usize <-- future add-on
-
-}
-
-impl Textov {
-    // constructor, will fill up all data members with helper functions
-    pub fn new(filepath: String) {
-        todo!();
-    }
-    // splits textfile by sentences and stores into data member(sentences)
-    pub fn split_sentences(filepath: String) -> Vec<String> {
-        //let results: HashMap<usize, usize> = runner::run(filepath);
-        let cleaned_contents: String = fs::read_to_string(filepath).expect("Something went wrong reading the file")
-        let sentences: Vec<String> = Vec::new();
-        let curr : String = String::new();
-        for i in 0..cleaned_contents.len() {
-            curr = curr + cleaned_contents[i];
-            if cleaned_contents[i] == "." || cleaned_contents[i] == "!"  cleaned_contents[i] == "?" {
-                sentence.add(curr);
-                curr = String::new();
-            }
-        }
-        return sentences;
-    }
-    // counts unique phrases from sentences data member
-    pub fn count_unique_phrases(self) {
-        todo!();
-    }
-    // creates phrase->idx maps
-    pub fn fill_maps(self) {
-        todo!();
-    }
-    // populates markov matrix 
-    pub fn fill_matrix(self) {
-        todo!();
-    }
-    // normalizes a vector with norm p=1
-    // normalize vector
-    pub fn normalize_vector(self, vect: &mut Vec<f64>) {
-        let sum: f64 = vect.iter().sum();
-        for i in 0..vect.len() {
-            vect[i] /= sum;
-        }
-    }
-    // normalizes markov matrix by row
-    pub fn normalize_matrix(self) {
-        for i in 0..self.markov_matrix.len() {
-            self.normalize_vector(&mut self.markov_matrix[i]);
-        }
-    }
-    // make weighted random choice from a vector
-    pub fn make_weighted_choice(self, vect: &Vec<f64>) -> usize {
-        let mut rng = rand::thread_rng();
-        let mut cum_prob: f64 = 0.0;
-        let mut choice: usize = 0;
-        let rand_prob: f64 = rng.gen();
-        for i in 0..vect.len() {
-            cum_prob += vect[i];
-            if rand_prob < cum_prob {
-                choice = i;
-                break;
-            }
-        }
-        choice
-    }
-    // generates a sentence from markov matrix
-    pub fn generate_sentence(self) -> String {
-        todo!();
-    }
-    // generates text with num_sentences from markov matrix
-    pub fn generate_text(self, num_sentences: usize) -> String {
-        todo!();
-    }
-
-}
-
-
-/*
-extern crate rand;
-
-use std::collections::HashMap;
 use std::fs;
 
 use rand::Rng;
@@ -237,6 +147,3 @@ pub fn make_weighted_choice(vect: &Vec<f64>) -> usize {
     }
     choice
 }
-
-
-*/
--- textov/src/main.rs
+++ textov/src/main.rs
@@ -1,14 +1,8 @@
+use textov::Textov;
+
 fn main() {
-    println!("Hello, world!");
+    let tev = Textov::new("datasets/green-eggs-and-ham.txt".to_string());
+    tev.print_all();
+    println!("{}", tev.generate_text(5));
+
 }
-
-
-
-// use textov::Textov;
-
-// fn main() {
-//     let tev = Textov::new("datasets/green-eggs-and-ham.txt".to_string());
-//     tev.print_all();
-//     println!("{}", tev.generate_text(5));
-
-// }
