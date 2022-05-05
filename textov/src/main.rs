7e22147a5e985a8c8400e7060169b9e1710a723d
pravshot <praveenkalva@icloud.com>
2022-05-04 22:36:47 -0500

    changes to main

--- textov/src/main.rs
+++ textov/src/main.rs
@@ -1,12 +1,6 @@
 use textov::Textov;
 
 fn main() {
-    let start = std::time::Instant::now();
-    let tev = Textov::new("datasets/long-example.txt".to_string());
-    tev.print_all();
-    println!("{}", tev.generate_sentence());
-    let end = std::time::Instant::now();
-    println!("{}", end.duration_since(start).as_millis());
+    let tev = Textov::new("datasets/goldilocks.txt".to_string());
+    println!("{}", tev.generate_text(5));
 }
-// 20.92 without concurrency
-// 17.92 with concurrency
