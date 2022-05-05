f823c493fc97696e69e1cff0488ec6d9414a41d8
pravshot <praveenkalva@icloud.com>
2022-05-05 07:45:55 -0500

    added commits and renamed dataset files

--- textov/src/main.rs
+++ textov/src/main.rs
@@ -1,6 +1,10 @@
 use textov::Textov;
 
 fn main() {
+    // Create a new Textov instance with the filepath to the text file
+    // this will be the dataset that will be used to generate sentences
     let tev = Textov::new("datasets/goldilocks.txt".to_string());
+    // We can use tev.generate_sentence() to generate a sentence or 
+    // tev.generate_text(usize) to generate multiple sentences and combine them together
     println!("{}", tev.generate_text(5));
 }
